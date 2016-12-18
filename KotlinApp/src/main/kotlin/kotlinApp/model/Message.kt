a89df2b35e6b4e8ee84e568dfa2358b3321d9539
Bastien7 <bastien.ladron@gmail.com>
2016-12-18 15:10:09 +0100

    Implemented a FormattedMessage class to help the API serialization.

--- KotlinApp/src/main/kotlin/kotlinApp/model/Message.kt
+++ KotlinApp/src/main/kotlin/kotlinApp/model/Message.kt
@@ -1,8 +1,17 @@
 package kotlinApp.model
 
+import java.sql.Timestamp
 import java.time.LocalDateTime
 import java.util.UUID
 
-data class Message(val uuid: UUID, val content: String, val date: LocalDateTime) {
+data class Message(val id: UUID, val content: String, val date: LocalDateTime) {
 	constructor(content: String) : this(UUID.randomUUID(), content, LocalDateTime.now())
+	constructor(message: MessageFormatted) : this(message.id, message.content, message.timestamp.toLocalDateTime())
+	constructor() : this("")
+
+	fun formatted(): MessageFormatted = MessageFormatted(this.id, this.content, Timestamp.valueOf(this.date))
+}
+
+data class MessageFormatted(val id: UUID, val content: String, val timestamp: Timestamp) {
+	constructor() : this(UUID.randomUUID(), "", Timestamp.valueOf(LocalDateTime.now()))
 }
